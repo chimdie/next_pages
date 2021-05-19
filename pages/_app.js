32389c718336049bfa7967c32c51710dea0ae793
chimdie <nzechimdi@yahoo.com>
2021-05-19 17:04:30 +0100

    Added the _app.js file
    Created the _app.js file to help modify how the layout/home page looks like and to be able to use global css.

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import "./styles/global.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}

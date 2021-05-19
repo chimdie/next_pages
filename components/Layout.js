e719ddfdf721b90f613510850f9f3bb75e8dc943
chimdie <nzechimdi@yahoo.com>
2021-05-19 16:58:09 +0100

    layout

--- components/Layout.js
+++ components/Layout.js
@@ -13,42 +13,6 @@ const Layout = (props, children) => {
       <Header appTitle={appTitle} />
       <div className="Content">{props.children}</div>
       <NavBar navButtons={navButtons} />
-
-      <style jsx global>{`
-      * {
-        box-sizing: border-box;
-      }
-
-      html,
-      body,
-      #__next {
-        height: 100%;
-        width: 100%;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-          "Helvetica Neue", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
-
-      .Layout {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        width: 100%;
-      }
-
-      .Content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-      }
-    `}</style>
     </div>
   );
 };
